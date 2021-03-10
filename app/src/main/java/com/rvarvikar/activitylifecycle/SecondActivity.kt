4a9d584e98b485a1d441050d23d629b7e9cafbb7
Rushikesh  Arvikar <rushikesharvikar@gmail.com>
2021-03-10 16:30:20 +0530

    Update SecondActivity.kt

--- app/src/main/java/com/rvarvikar/activitylifecycle/SecondActivity.kt
+++ app/src/main/java/com/rvarvikar/activitylifecycle/SecondActivity.kt
@@ -5,67 +5,33 @@ import android.util.Log
 import java.util.*
 
 class SecondActivity: AppCompatActivity() {
-    private
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    val TAG :String = javaClass.simpleName
+    private val TAG :String = javaClass.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
         printLog(object{}.javaClass.enclosingMethod.name)
     }
-
     override fun onStart() {
         super.onStart()
         printLog(object{}.javaClass.enclosingMethod.name)
     }
-
     override fun onResume() {
         super.onResume()
         printLog(object{}.javaClass.enclosingMethod.name)
     }
-
     override fun onPause() {
         super.onPause()
         printLog(object{}.javaClass.enclosingMethod.name)
     }
-
     override fun onStop() {
         super.onStop()
         printLog(object{}.javaClass.enclosingMethod.name)
     }
-
     override fun onRestart() {
         super.onRestart()
         printLog(object{}.javaClass.enclosingMethod.name)
     }
-
     override fun onDestroy() {
         super.onDestroy()
         printLog(object{}.javaClass.enclosingMethod.name)
